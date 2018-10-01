41e6656a027581def121a86c647848e34172967e
dskurikhin <d.skurikhin@eastwind.ru>
2018-10-01 18:33:25 +0500

    start dbPreparation

--- src/Server.hs
+++ src/Server.hs
@@ -6,12 +6,17 @@ module Server
 
 import Web.Scotty (scotty)
 import Routes (routes)
-import Database.PostgreSQL.Simple (connectPostgreSQL)
+import Database.PostgreSQL.Simple (Connection, connectPostgreSQL, execute)
+
+usersTableName = "users"
+
+prepareDb :: Connection -> IO Connection
+prepareDb conn = (execute conn "create table if not exists ? (email varchar(50), password varchar(200))", (,) usersTableName) >> return conn
 
 port = 8080
--- TODO: CREATE DB and TABLE on start
+
 startServer :: IO ()
 startServer =
     (putStrLn $ "Server started at " ++ (show port)) >>
-    (connectPostgreSQL "host='psql' port=5432 user='postgres' dbname='auth'") >>= \dbConn ->
-        scotty port (routes dbConn)
+    (connectPostgreSQL "host='psql' port=5432 user='postgres' dbname='auth'") >>=
+        \dbConn -> prepareDb >> scotty port (routes dbConn)
