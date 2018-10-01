72d2a30d70fd1d2ff121afd72a8c844d0d647998
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-10-02 00:20:46 +0500

    create table on start

--- src/Server.hs
+++ src/Server.hs
@@ -5,13 +5,14 @@ module Server
     ) where
 
 import Web.Scotty (scotty)
+import Control.Monad.IO.Class (liftIO)
 import Routes (routes)
 import Database.PostgreSQL.Simple (Connection, connectPostgreSQL, execute)
 
-usersTableName = "users"
-
 prepareDb :: Connection -> IO Connection
-prepareDb conn = (execute conn "create table if not exists ? (email varchar(50), password varchar(200))", (,) usersTableName) >> return conn
+prepareDb conn =
+    (execute conn "create table if not exists users (email varchar(50), password varchar(200))" ()) >>
+        return conn
 
 port = 8080
 
@@ -19,4 +20,4 @@ startServer :: IO ()
 startServer =
     (putStrLn $ "Server started at " ++ (show port)) >>
     (connectPostgreSQL "host='psql' port=5432 user='postgres' dbname='auth'") >>=
-        \dbConn -> prepareDb >> scotty port (routes dbConn)
+        \dbConn -> liftIO $ prepareDb dbConn >> scotty port (routes dbConn)
