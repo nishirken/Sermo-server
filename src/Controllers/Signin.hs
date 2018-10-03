88265f57db812f9619a1dcba216f525edbdf4a62
dskurikhin <d.skurikhin@eastwind.ru>
2018-10-03 16:03:54 +0500

    start check email exists

--- src/Controllers/Signin.hs
+++ src/Controllers/Signin.hs
@@ -5,7 +5,7 @@ module Controllers.Signin where
 import Data.Text.Lazy (pack, Text)
 import qualified Data.Text.Lazy as T
 import Web.Scotty (html, ActionM, param, text, redirect, rescue)
-import Database.PostgreSQL.Simple (Connection, execute)
+import Database.PostgreSQL.Simple (Connection, execute, Only (..), query)
 import Control.Monad.IO.Class (liftIO)
 import Lucid (renderText)
 import Views.SigninPage (signinPageView)
@@ -24,11 +24,26 @@ validateEmailLength :: Text -> Either Text Text
 validateEmailLength email =
     if T.length email < 51 then Right email else Left "Email length incorrect, max length is 50"
 
-validate :: Signin -> Either Text Signin
-validate (Signin email' password' repeatedPassword') =
-    validateEmailLength email' >>= \email ->
-        validatePasswordMatch password' repeatedPassword' >>= validatePasswordLength >>=
-            \password -> Right $ Signin email password password
+getUserCredsByEmail :: Connection -> Text -> IO [(Text, Text)]
+getUserCredsByEmail dbConn email = query dbConn "select email, password from users where email = ?" (Only email)
+
+validateEmailAlreadyExists :: Connection -> Text -> IO (Either Text Text)
+validateEmailAlreadyExists dbConn email =
+    fmap checkIfEmpty $ getUserCredsByEmail dbConn email
+        where
+            checkIfEmpty rows = case (length rows) of
+                0 -> Right email
+                _ -> Left $ pack "Email already exists"
+
+validate :: Connection -> Signin -> Either Text Signin
+validate dbConn (Signin email' password' repeatedPassword') = do
+    existedEmail <- validateEmailAlreadyExists dbConn email'
+    case existedEmail of
+        (Right e) -> do
+            email <- validateEmailLength e
+            password <- validatePasswordMatch password' repeatedPassword' >>= validatePasswordLength
+            Right $ Signin email password password
+        (Left err) -> Left err
 
 getParam :: Text -> ActionM Text
 getParam paramName = do
@@ -40,6 +55,6 @@ signinController dbConn = do
     email <- getParam "email" :: ActionM Text
     password <- getParam "password"
     repeatedPassword <- getParam "repeatedPassword"
-    case (validate $ Signin email password repeatedPassword) of
+    case (validate dbConn $ Signin email password repeatedPassword) of
         (Right _) -> (liftIO $ execute dbConn "insert into users values (?,?)" (email, password)) >> redirect "/"
         (Left errorMessage) -> html . renderText $ signinPageView $ FormPageView errorMessage
