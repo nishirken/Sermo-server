ebe90ed166c2edb020f1aa8e466d8988c871548a
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2018-10-26 04:17:59 +0500

    add cors

--- src/Routes.hs
+++ src/Routes.hs
@@ -5,6 +5,7 @@ module Routes (routes) where
 
 import Web.Scotty (get, post, html, ScottyM, notFound, middleware, status)
 import Network.Wai.Middleware.Static (static)
+import Network.Wai.Middleware.Cors (simpleCors)
 import Database.PostgreSQL.Simple (Connection)
 import Handlers.LogIn
 import Handlers.SignIn
@@ -15,6 +16,7 @@ import Config (Config (..))
 routes :: Connection -> Config -> ScottyM ()
 routes dbConn Config{ authKey }  = do
     middleware static
+    middleware simpleCors
     middleware $ authMiddleware authKey
     post "/login" $ logInHandler authKey dbConn
     post "/signin" $ signInHandler authKey dbConn
