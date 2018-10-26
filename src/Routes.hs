7380bd9b74aa6c1378e0bf5408091c78bf670e93
dskurikhin <d.skurikhin@eastwind.ru>
2018-10-26 10:35:52 +0500

    add content-type to cors

--- src/Routes.hs
+++ src/Routes.hs
@@ -5,18 +5,31 @@ module Routes (routes) where
 
 import Web.Scotty (get, post, html, ScottyM, notFound, middleware, status)
 import Network.Wai.Middleware.Static (static)
-import Network.Wai.Middleware.Cors (simpleCors)
+import Network.Wai.Middleware.Cors (cors, CorsResourcePolicy (..))
 import Database.PostgreSQL.Simple (Connection)
 import Handlers.LogIn
 import Handlers.SignIn
 import Handlers.Auth (authMiddleware)
 import Handlers.Utils (makeStatus)
+import Data.Text.Encoding (encodeUtf8)
+import Data.CaseInsensitive (mk)
 import Config (Config (..))
+
+corsConfig = CorsResourcePolicy {
+  corsOrigins = Nothing
+  , corsMethods = encodeUtf8 <$> ["POST", "GET"]
+  , corsRequestHeaders = (mk . encodeUtf8) <$> ["Accept", "Accept-Language", "Content-Language", "Content-Type"]
+  , corsExposedHeaders = Just $ (mk . encodeUtf8) <$> ["Access-Control-Allow-Origin"]
+  , corsMaxAge = Nothing
+  , corsVaryOrigin = False
+  , corsRequireOrigin = True
+  , corsIgnoreFailures = False
+}
 
 routes :: Connection -> Config -> ScottyM ()
 routes dbConn Config{ authKey }  = do
     middleware static
-    middleware simpleCors
+    middleware $ cors $ \_ -> Just corsConfig
     middleware $ authMiddleware authKey
     post "/login" $ logInHandler authKey dbConn
     post "/signin" $ signInHandler authKey dbConn
