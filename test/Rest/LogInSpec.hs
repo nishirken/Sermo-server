81a0fcaceed1c05eee71bd0bde65662383bab512
Дмитрий Скурихин <dmitry.skurihin@monopoly.su>
2019-04-19 18:10:24 +0300

    add test for verification

--- test/Rest/LogInSpec.hs
+++ test/Rest/LogInSpec.hs
@@ -2,7 +2,7 @@ module Rest.LogInSpec where
 
 import Test.Hspec (context, describe, it, SpecWith, shouldBe)
 import Test.QuickCheck (Property, property, generate, Gen, arbitrary)
-import Test.QuickCheck.Monadic (monadicIO, run)
+import Test.QuickCheck.Monadic (monadicIO, run, assert)
 import RestHandlers.LogIn (verify)
 import qualified Data.Text as Text
 import Data.Text.Encoding (encodeUtf8, decodeUtf8)
@@ -11,8 +11,7 @@ import qualified Crypto.Scrypt as Crypto
 logInSpec :: SpecWith ()
 logInSpec =
   describe "LogIn" $ do
-    it "Password verification" $ monadicIO $ do
-      password <- generate (arbitrary :: Text.Text)
+    it "Password verification" $ property $ monadicIO $ do
+      password <- run $ Text.pack <$> generate (arbitrary :: Gen String)
       passwordHash <- run $ Crypto.encryptPassIO' (Crypto.Pass . encodeUtf8 $ password)
-      verify password ((decodeUtf8 . Crypto.getEncryptedPass) passwordHash) `shouldBe` True
-
+      assert $ verify password $ (decodeUtf8 . Crypto.getEncryptedPass) passwordHash
