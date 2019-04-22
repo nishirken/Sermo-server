aa94a985cd34d1c155b2a75d87bd487cf3069b69
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-04-23 01:04:46 +0300

    proceed auth spec

--- test/Rest/AuthSpec.hs
+++ test/Rest/AuthSpec.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module Rest.AuthSpec (isAuthorizedHandler, createToken) where
+module Rest.AuthSpec (authSpecIO) where
 
 import Test.Hspec (context, describe, it, Spec, shouldReturn)
-import Rest.Utils (withMockedToken, logInPreparation, preparation)
+import Rest.Utils (withMockedToken, preparation)
 import Test.Hspec.Wai (with, post, shouldRespondWith)
 import Test.Hspec.Wai.JSON (json)
 import Data.Text.Encoding (encodeUtf8)
-import Db (getUserCredsByEmail)
+import qualified Db
 import RestHandlers.Auth (createToken, isAuthorizedHandler, isTokenValid)
+import Control.Monad.IO.Class (liftIO)
 
-authSpec :: Spec
-authSpec = with (fst <$> preparation) $ describe "Authorization" $
+authSpecIO :: Spec
+authSpecIO = with (fst <$> preparation) $ describe "Authorization" $
   it "Not authorize with wrong token" $
-    post (encodeUtf8 "/auth") [json|{ token:"ababa" }|] `shouldRespondWith`
-    [json|{ data: false, error: null }|]
+    post (encodeUtf8 "/auth") [json|{ token:"ababa" }|] `shouldRespondWith` [json|{ data: false, error: null }|]
+  it "Authorize with right token" $ do
+    token <- liftIO (do
+      rows <- Db.getUserCredsByEmail
+      pure $ generatedToken "authKey" (fst $ head rows))
+    post (encodeUtf8 "/auth") [json|{ token:"" }|] `shouldRespondWith`
+    [json|{ data: true, error: null }|]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,10 @@
 import Test.Hspec (hspec)
 import Rest.LogInSpec (logInSpec)
 import Rest.SignInSpec (signInSpecIO)
+import Rest.AuthSpec (authSpecIO)
 
 main :: IO ()
 main = hspec $ do
-  logInSpec
-  signInSpecIO
+  -- logInSpec
+  -- signInSpecIO
+  authSpecIO
