c217625cac90be2557a73bf3a2810eb2d5aa2ffa
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-04-28 12:58:48 +0300

    add encode tests for models

--- test/ModelsSpec.hs
+++ test/ModelsSpec.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module ModelsSpec (modelsSpec) where
 
-import Test.Hspec (context, describe, it, Spec, shouldReturn)
+import Test.Hspec (context, describe, it, Spec, shouldBe)
 import Models.Index (JSONError (..), JSONResponse (..), SuccessResponse (..), GraphQLRequest (..), LoginRequest (..))
 import Models.TokenObject (TokenObject (..))
 import Test.QuickCheck (property)
 import ArbitraryInstances
 import qualified Data.Yaml as Yaml
 import qualified Data.Aeson as Aeson
+import Data.Aeson.QQ (aesonQQ)
 
 type TestJSON a = a -> Bool
 
@@ -17,9 +21,28 @@ testJSON model = case (Aeson.decode . Aeson.encode) model of
 
 modelsSpec :: Spec
 modelsSpec = describe "Common rest models" $ do
-  it "JSONError model" $ property (testJSON :: TestJSON JSONError)
-  it "JSONResponse model" $ property (testJSON :: TestJSON (JSONResponse GraphQLRequest))
-  it "SuccessResponse model" $ property (testJSON :: TestJSON SuccessResponse)
-  it "LoginRequest model" $ property (testJSON :: TestJSON LoginRequest)
-  it "GraphQLRequest model" $ property (testJSON :: TestJSON GraphQLRequest)
-  it "TokenObject model" $ property (testJSON :: TestJSON TokenObject)
+  context "JSONError model" $ do
+    it "JSON encode decode" $ property (testJSON :: TestJSON JSONError)
+    it "Success decode" $
+      Aeson.toJSON (JSONError 200 $ Just "message") `shouldBe` [aesonQQ|{ code: 200, message: "message" }|]
+  context "JSONResponse model" $ do
+    it "JSON encode decode" $ property (testJSON :: TestJSON (JSONResponse GraphQLRequest))
+    it "Success decode" $
+      Aeson.toJSON (JSONResponse (Just $ GraphQLRequest "token22" "graphQLBody") Nothing) `shouldBe`
+        [aesonQQ|{ data: { token: "token22", body: "graphQLBody" }, error: null }|]
+  context "SuccessResponse model" $ do
+    it "JSON encode decode" $ property (testJSON :: TestJSON SuccessResponse)
+    it "Success decode" $
+      Aeson.toJSON (SuccessResponse True) `shouldBe` [aesonQQ|{ success: true }|]
+  context "LoginRequest model" $ do
+    it "JSON encode decode" $ property (testJSON :: TestJSON LoginRequest)
+    it "Success decode" $ Aeson.toJSON (LoginRequest "mail@test.ru" "123456") `shouldBe`
+      [aesonQQ|{ email: "mail@test.ru", password: "123456" }|]
+  context "GraphQLRequest model" $ do
+    it "JSON encode decode" $ property (testJSON :: TestJSON GraphQLRequest)
+    it "Success decode" $ Aeson.toJSON (GraphQLRequest "token22" "graphQLBody") `shouldBe`
+      [aesonQQ|{ token: "token22", body: "graphQLBody" }|]
+  context "TokenObject model" $ do
+    it "JSON encode decode" $ property (testJSON :: TestJSON TokenObject)
+    it "Success decode" $ 
+      Aeson.toJSON (TokenObject "token22") `shouldBe` [aesonQQ|{ token: "token22" }|]
