dfd0a873ea505da086126a55707613611eb421ed
Dmitrii Skurikhin <dmitrii.sk@gmail.com>
2019-05-28 08:13:30 +0300

    change Id type in user response

--- src/Main/Schemas.hs
+++ src/Main/Schemas.hs
@@ -10,6 +10,7 @@ import GraphQL (interpretAnonymousQuery)
 import GraphQL.API (Argument, Object, Field, (:>), List)
 import GraphQL.Resolver (Handler, (:<>) (..), ResolverError (..), Result (..))
 import GraphQL.Value.ToValue (toValue)
+import Data.Int (Int32)
 import qualified Utils
 import qualified Database.PostgreSQL.Simple as PSQL
 import qualified Data.Text as Text
@@ -17,13 +18,13 @@ import qualified Db
 import qualified Models.DbUser as DbUser
 
 type Friend = Object "Friend" '[]
-  '[ Field "id" Text.Text, Field "email" Text.Text ]
+  '[ Field "id" Int32, Field "email" Text.Text ]
 
 type User = Object "User" '[]
-  '[ Field "id" Text.Text, Field "email" Text.Text, Field "friends" (List Text.Text) ]
+  '[ Field "id" Int32, Field "email" Text.Text ]
 
 type RootQuery = Object "RootQuery" '[]
-  '[ Argument "id" Text.Text :> Field "user" (Maybe User) ]
+  '[ Argument "id" Int32 :> Field "user" (Maybe User) ]
 
 friendHandler :: DbUser.DbUser -> Handler IO Text.Text
 friendHandler DbUser.DbUser {..} = pure _email
@@ -31,17 +32,16 @@ friendHandler DbUser.DbUser {..} = pure _email
 friendsHandler :: [DbUser.DbUser] -> Handler IO (List Text.Text)
 friendsHandler friends = pure $ map friendHandler friends
 
-userHandler :: Int -> Text.Text -> [DbUser.DbUser] -> Handler IO User
-userHandler id email friends = pure $
-  pure ((Text.pack . show) id) :<> pure email :<> friendsHandler friends
+userHandler :: Int32 -> Text.Text -> [DbUser.DbUser] -> Handler IO User
+userHandler id email friends = pure (pure id :<> pure email)
 
 rootQueryHandler :: PSQL.Connection -> Handler IO RootQuery
 rootQueryHandler dbConn = pure $ \userId -> do
-  result <- Db.getUserById dbConn (read @Int $ Text.unpack userId)
+  result <- Db.getUserById dbConn (fromIntegral userId :: Int)
   case result of
     [DbUser.DbUser {..}] -> do
       friends <- Db.getUsersByIds dbConn _friendsIds
-      pure $ Just $ userHandler _id _email friends
+      pure $ Just $ userHandler (fromIntegral _id :: Int32) _email friends
     _ -> pure Nothing
 
 interpretRootQuery :: Utils.QueryHandler
